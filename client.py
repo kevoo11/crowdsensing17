7e7d7c797dfd9095aab4e2b8478129edf693fa40
kevin <kevoo11@uw.edu>
2017-04-17 10:31:06 -0700

    updated the server, server-database working; need to update the data protocol

---
+++ client.py
@@ -0,0 +1,23 @@
+import socket
+import sys
+
+HOST, PORT = '108.179.151.79' , 10000
+data = " ".join(sys.argv[1:])
+
+# Create a socket (SOCK_STREAM means a TCP socket)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    # Connect to server and send data
+    sock.connect((HOST, PORT))
+    sock.sendall(bytes(data + "\n", "utf-8"))
+
+    # Receive data from the server and shut down
+    received = str(sock.recv(1024), "utf-8")
+finally:
+    sock.close()
+
+print("Sent:     {}".format(data))
+print("Received: {}".format(received))
+print(type(received))
+
